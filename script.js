a3ea655bf341d2d0fda3fce4c1245bed3018754c
kumar <kumareffect@gmail.com>
2025-03-28 09:31:15 +0530

    done

---
+++ script.js
@@ -0,0 +1,12 @@
+function triangleShape(n) {
+    let string = "";
+    for(let i=0; i<=n; i++) {
+        for(let j=0; j<=i; j++) {
+            string += "*";
+        }
+        string += "\n";
+    }
+    console.log(string);
+}
+
+triangleShape(6);
